2b1cf5d360b421a59effc314aa635742c5930611
qiaoyu.yu@gmail.com <qiaoyu.yu@gmail.com@62ad2cea-32ad-3ff6-ad92-f41438b3c8a2>
2012-11-10 17:51:58 +0000

    initial checkin

---
+++ DAO/userDAO.php
@@ -0,0 +1,26 @@
+<?php
+require_once "common.php";
+
+function register_user($user_name,$email,$password)
+{
+	$md5_password = md5($password);
+	$query = "INSERT INTO Users(u_name, u_email, u_password)  VALUES (:user_name,:email,:md5_password)";
+	
+	$stmt = prepare_statement($query);
+
+	oci_bind_by_name($stmt,":user_name",$user_name);
+	oci_bind_by_name($stmt,":email",$email);
+	oci_bind_by_name($stmt,":md5_password",$md5_password);	
+	
+	oci_execute($stmt);
+
+	$err = oci_error($stmt);
+	if ($err){
+		echo $err['message'];
+	}
+}
+
+
+oci_close($conn);
+?>
+
