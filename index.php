bedeb211c914153e2181a4113bcf39f8fdac60e3
qiaoyu.yu@gmail.com <qiaoyu.yu@gmail.com@62ad2cea-32ad-3ff6-ad92-f41438b3c8a2>
2012-11-11 05:02:55 +0000

    Changed index page.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
@@ -8,88 +8,69 @@
 		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</head>
+
+
 	<body>
 		<div id="wrapper">
 			<div id="header">
 				<div id="logo">
 					<h1><a href="index.php">SNS Portal</a></h1>
 				</div>
-				<div id="menu">
-					<ul>
-					<li>logon</li>
-					<li>logoff</li>
-					</ul>
-					<br class="clearfix" />
-				</div>
 			</div>
 			<div id="page">
 				<div id="sidebar">
 					<div class="box">
-						<h3>Natoque mattis</h3>
+						<form id='login' action='checklogin.php' method='post' accept-charset='UTF-8'>
+						<h3>Log In</h3>
 						<p>
-							Morbi lorem ante sodales primis ultrices justo. Enim id cubilia arcu posuere arcu phasellus eleifend.
+							SNS Portal is a website through which you can collectively acquire all SNS activities on other websites.  
 						</p>
-						<ul class="list">
-							<li class="first"><a href="#">Lobortis magna nisi parturient</a></li>
-							<li><a href="#">Fusce laoreet odio pulvinar</a></li>
-							<li class="last"><a href="#">Turpis lacus lectus iaculis</a></li>
-						</ul>
-					</div>
-					<div class="box">
-						<h3>Fusce quam</h3>
-						<ul class="list">
-							<li class="first"><a href="#">Penatibus at dignissim</a></li>
-							<li><a href="#">Feugiat vivamus convallis integer</a></li>
-							<li><a href="#">Nunc morbi semper commodo</a></li>
-							<li><a href="#">Lorem vestibulum luctus</a></li>
-							<li class="last"><a href="#">Nisl dis rhoncus mattis</a></li>
-						</ul>
-					</div>
+						<ul>
+							<li>Email  <input style="color: rgb(51, 51, 51);" name="email" id="email" tabindex="1" value="" type="text" /></li>
+							<li>Password <input style="color: rgb(51, 51, 51);" name="password" id="password" tabindex="1" value="" size="17" type="password" /></li>
+						</ul>	
+						<input type='submit' name='Submit' value='Submit' />					
+
+						</form>
+						</div>
 				</div>
 				<div id="content">
 					<div class="box">
-						<h2>Welcome to Accumen</h2>
-						<img class="alignleft" src="images/pic01.jpg" width="200" height="180" alt="" />
+						<h2>Welcome to SNS Portal</h2>
 						<p>
-							This is <strong>Accumen</strong>, a free, fully standards-compliant CSS template by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>. The images used in this template are from <a href="http://fotogrph.com/">Fotogrph</a>. This free template is released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attributions 3.0</a> license, so you are pretty much free to do whatever you want with it (even use it commercially) provided you keep the footer credits intact. Aside from that, have fun with it :)
+							This is <strong>SNS Portal</strong>. Welcome to register.
 						</p>
 					</div>
 					<div class="box">
-						<h3>Nullam curae integer</h3>
-						<p>
-							Congue quam posuere elit adipiscing varius tellus. Consequat porttitor dolor sed viverra cum congue. Varius primis auctor est nisl at mi quam. Ante libero arcu ridiculus blandit placerat. Sociis in consequat suscipit felis vivamus odio phasellus. Elementum pellentesque vestibulum gravida morbi turpis ante nullam. Pellentesque placerat porta ipsum.
-						</p>
-					</div>
-					<div class="box">
-						<h3>Accumsan euismod gravida pellentesque</h3>
-						<p>
-							Sociis dictum mauris ultricies suspendisse in quisque. Massa non morbi magna eget. Cubilia aliquet duis sapien ac nascetur fringilla ullamcorper. Auctor est odio rhoncus pharetra purus mattis. Ultrices vulputate lacus ridiculus imperdiet cursus. Tellus ridiculus morbi fringilla volutpat mi magna aliquam.
-						</p>
+						<form id='register' action='register.php' method='post' accept-charset='UTF-8'>
+						<ul>
+                            <li>Email: <input style="color: rgb(51, 51, 51);" name="email" id="email" tabindex="1" value="" type="text"></li>
+                            <li>Password: <input style="color: rgb(51, 51, 51);" name="password" id="password" tabindex="1" value="" type="password"></li>
+						    <li>Confirm Password: <input style="color: rgb(51, 51, 51);" name="password" id="password" tabindex="1" value="" type="password"></li>
+                            <li>Name: <input style="color: rgb(51, 51, 51);" name="password" id="password" tabindex="1" value="" type="text"></li>
+						</ul>
+				
+						<input type='submit' name='Submit' value='Register' />
+
+					</form>
+
 					</div>
 					<br class="clearfix" />
 				</div>
 				<br class="clearfix" />
 			</div>
 			<div id="page-bottom">
-				<div id="page-bottom-sidebar">
-					<h3>Elementum quam</h3>
-					<ul class="list">
-						<li class="first"><a href="#">Scelerisque diam</a></li>
-						<li><a href="#">Varius tellus facilisis</a></li>
-						<li class="last"><a href="#">Cursus non laoreet</a></li>
-					</ul>
-				</div>
 				<div id="page-bottom-content">
-					<h3>Dictum sociis</h3>
+					<h3>Acknowledgement</h3>
 					<p>
-						Libero phasellus nulla justo magnis praesent vitae magna. Nulla ullamcorper mollis porttitor ligula interdum justo. Penatibus arcu morbi magnis amet primis dolor viverra. Ornare vestibulum quam enim. Dignissim ultricies natoque nibh justo est mus tempus. Curae sagittis lobortis suscipit eget sodales elit eget. Faucibus ridiculus vitae hendrerit malesuada diam. Natoque sed facilisis faucibus arcu. Malesuada viverra sit at integer. Nunc erat accumsan risus. Magna nunc est faucibus.
+						This site is built for Columbia University W4111 2012 Fall Project.
 					</p>
 				</div>
 				<br class="clearfix" />
 			</div>
 		</div>
 		<div id="footer">
-			&copy; 2012 Untitled | Design by <a href="http://www.freecsstemplates.org/">FCT</a> | Images by <a href="http://fotogrph.com/">Fotogrph</a>
+			&copy; 2012 SNS Portal | Design by Yu Qiao (yq2145) and Cheng Xiang (cx2142) </a>
 		</div>
 	</body>
 </html>
