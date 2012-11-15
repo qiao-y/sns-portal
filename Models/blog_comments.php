416a0582207563d32bd8ba90379a40523b920db0
qiaoyu.yu@gmail.com <qiaoyu.yu@gmail.com@62ad2cea-32ad-3ff6-ad92-f41438b3c8a2>
2012-11-15 02:31:05 +0000

    Fixed the blog comments

--- Models/blog_comments.php
+++ Models/blog_comments.php
@@ -8,11 +8,13 @@ class blog_comments
 	public $bcid;
 	public $uid;
 	public $body;
+	public $date;
 
-	function __construct($bcid,$uid,$body){
+	function __construct($bcid,$uid,$body,$date){
 		$this->bcid = $bcid;
 		$this->uid = $uid;
 		$this->body = $body;
+		$this->date = $date;
 	}
 
 }
