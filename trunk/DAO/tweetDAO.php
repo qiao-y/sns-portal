9d6f6bc7a28c0da9fac66e8b7dac17036407c0bc
qiaoyu.yu@gmail.com <qiaoyu.yu@gmail.com@62ad2cea-32ad-3ff6-ad92-f41438b3c8a2>
2012-11-17 05:43:09 +0000

    Add twitter support and multiple colors

---
+++ trunk/DAO/tweetDAO.php
@@ -0,0 +1,46 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once "common.php";
+require_once "$root/Models/tweet.php";
+
+function get_tweet_by_uid($userid)
+{
+	global $conn;
+	$query = "select * from tweet where u_id = " . $userid . "order by t_timestamp";
+	$stmt = exec_query($query);
+
+	$result = array();
+	
+	while ($row = oci_fetch_array($stmt,OCI_ASSOC)){
+		$item = new status($row["T_ID"],$row["U_ID"],$row["S_CONTENT"],$row["S_TIMESTAMP"]);
+		array_push($result,$item);
+	}
+	oci_close($conn);
+	return $result;	
+}	
+
+
+function get_tweet_by_tid($tid)
+{
+    global $conn;
+    $query = "select * from tweet where s_id = " . $tid . "order by t_timestamp";
+    
+    $stmt = exec_query($query);
+
+    $row = oci_fetch_array($stmt,OCI_ASSOC);
+
+	$result = new status($row["T_ID"],$row["U_ID"],$row["S_CONTENT"],$row["S_TIMESTAMP"]);
+
+	oci_close($conn);
+    return $result;
+}   
+
+
+
+//$res = get_status_by_uid(42);
+//var_dump($res);
+
+//var_dump(get_status_by_sid(10));
+
+?>
+
--- trunk/sharing_helper.php
+++ trunk/sharing_helper.php
@@ -13,20 +13,18 @@ function output_sharing($item,$friendid)
 	$username = get_user_name_by_id($friendid);
 	if ($item->category == 1){
 		$blog_title = get_blog_title_by_id($item->correspondingid); 
-		return "$username shared a <strong>blog<strong>: <a href=\"readblog.php?bid=$item->correspondingid\">$blog_title</a><br/>";
+		return "$username shared a <strong>blog</strong>: <a href=\"readblog.php?bid=$item->correspondingid\" style=\"background-color:rgb(56,69,138)\">$blog_title</a><br/>";
 	}
 	else if ($item->category == 2){
 		$status = get_status_by_sid($item->correspondingid)->content;
-		return "$username shared a <strong>status</strong>: <a href=\"status.php?friendid=$friendid&sid=$item->correspondingid\">$status</a><br/>";
+		return "$username shared a <strong>status</strong>: <a href=\"status.php?friendid=$friendid&sid=$item->correspondingid\"  style=\"background-color:rgb(56,69,138)\">$status</a><br/>";
 	}
 	else if ($item->category == 3){
 		$link = get_picture_by_pid($item->correspondingid)->link; 
 		
 		return 
 			"$username shared a <strong>picture</strong>".
-			"<div class=\"img\">".
-			"<a href=\"viewpicture.php?friendid=$friendid&pid=$item->correspondingid\"><img src=\"$link\" alt=\"NULL\" width=\"110\" height=\"90\"></a>".
-			"</div>";		
+			"<a href=\"viewpicture.php?friendid=$friendid&pid=$item->correspondingid\"><img src=\"$link\" alt=\"NULL\" width=\"110\" height=\"90\"></a><br/>";
 	}
 
 }
